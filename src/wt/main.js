39e3a213a5b11334739bd5ca09d39b758b101c8a
Marina Khitrovo <Marina_Khitrovo@epam.com>
2024-01-30 01:16:42 +0500

    wth task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import os from "node:os";
+import { Worker } from "node:worker_threads";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const file = path.join(__dirname, "worker.js");
+
+  const cpuDNumber = os.cpus().length;
+  const arr = [];
+  for (let i = 1; i <= cpuDNumber; i++) {
+    arr.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(file, {
+          workerData: 10 + i
+        });
+        worker.on("message", (msg) => {
+          resolve({ status: "resolved", msg });
+        });
+        worker.on("error", (err) => {
+          reject({ status: "error", err });
+        });
+      })
+    );
+  }
+
+  const result = await Promise.all(arr);
+  console.log(result);
 };
 
 await performCalculations();
