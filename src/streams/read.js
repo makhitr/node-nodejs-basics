2fc7bfd0223fef133416922a6c93eb6096294bb5
Marina Khitrovo <Marina_Khitrovo@epam.com>
2024-01-30 01:11:15 +0500

    stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "fs";
+import { stdout } from "process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files/fileToRead.txt");
+  const rs = createReadStream(file, "utf-8");
+  rs.pipe(stdout);
+  // Write your code here
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const ts = new Transform({
+    transform(chunk, enc, callback) {
+      const ch = chunk.toString().trim();
+      const reversedCh = ch.split("").reverse().join("");
+      callback(null, `${reversedCh}\n`);
+    }
+  });
+  stdin.pipe(ts).pipe(stdout);
+  // Write your code here
 };
 
 await transform();
