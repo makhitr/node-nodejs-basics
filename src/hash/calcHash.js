e58c95832b3ce167080b6d328cfe2f7c9675f14b
Marina Khitrovo <Marina_Khitrovo@epam.com>
2024-01-30 01:07:50 +0500

    hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+const { createHash } = await import("node:crypto");
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files/fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  hash.update(file);
+  console.log(hash.digest("hex"));
+  // Write your code here
 };
 
 await calculateHash();
