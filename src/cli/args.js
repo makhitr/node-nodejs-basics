e2cf8c1807a7218053a6da7442bb3b3b25913983
Marina Khitrovo <Marina_Khitrovo@epam.com>
2024-01-30 00:59:14 +0500

    cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
+    const args = process.argv.slice(2)
+    let result = []
+    args.map((arg, index) => {
+      if (arg.startsWith('--') && (index + 1) < args.length) {
+        result.push(`${arg.slice(2)} is ${args[index + 1]}`);
+      }
+    })
+    console.log(result.join(', '))
     // Write your code here 
-};
-
-parseArgs();
+  };
+  
+  
+  parseArgs()
+  
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
-};
-
-parseEnv();
+    Object.entries(process.env)
+      .filter((envVar) => envVar[0].slice(0, 4) === "RSS_")
+      .map((rssVar) => console.log(`${rssVar[0]}=${rssVar[1]}`));
+  };
+  
+  parseEnv();
+  
