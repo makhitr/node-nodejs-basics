8a528694e42805ea84dc6e7c3fe170a2fd6a7fd4
Marina Khitrovo <Marina_Khitrovo@epam.com>
2024-01-30 01:18:36 +0500

    cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { spawn } from 'node:child_process'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
+    const file = path.join(__dirname, 'files/script.js')
+    spawn('node', [file, ...args], {
+        stdio: [process.stdin, process.stdout, process.stderr]
+    });
     // Write your code here
 };
-
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
