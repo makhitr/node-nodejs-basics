aa39f212c5474b4f4b7237846f5e45f3c177aebf
sergkhit <s.khitrovo@gmail.com>
2025-10-26 23:04:37 +0500

    env tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-  // Write your code here
+  const prefix = "RSS_";
+
+  const result = Object.entries(process.env)
+    .filter(([envVar]) => envVar.startsWith(prefix))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(result);
 };
 
 parseEnv();
