0687238763e206a4de82625a5be478563247d767
sergkhit <s.khitrovo@gmail.com>
2025-10-26 23:49:30 +0500

    stream tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const filePath = path.join(__dirname, "files/fileToRead.txt");
+  const stream = createReadStream(filePath, "utf-8");
+  stream.pipe(stdout);
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
 const transform = async () => {
-  // Write your code here
+     const reverseStream = new Transform({
+        transform(chunk, enc, callback) {
+            const ch = chunk.toString().trim()
+            const reversedCh = ch.split('').reverse().join('');
+            callback(null, `${reversedCh}\n`)
+        }
+    })
+    stdin.pipe(reverseStream).pipe(stdout)
 };
 
 await transform();
